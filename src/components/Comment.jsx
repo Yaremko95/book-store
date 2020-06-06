e30ee36d3f4636a0845ba6229be1d216304d2b74
tetiana <tetianayaremko@gmail.com>
2020-06-07 00:11:27 +0200

    deletes comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,9 +2,27 @@ import React, {Component} from 'react';
 import Rating from "react-rating";
 import {FaRegStar, FaStar} from "react-icons/fa";
 import TimeAgo from "react-timeago/lib";
+import Button from "react-bootstrap/Button";
 
 class Comment extends Component {
+    constructor(props) {
+        super(props);
+    }
+    handleDelete =async(id) => {
+       
+        try {
+            let response = await fetch(`https://striveschool.herokuapp.com/api/comments/${id}`, {
+                "method": "DELETE",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Authorization": "Basic " + btoa('user27:ZGDWyFCA8umbgpvZ')}
 
+            })
+           this.props.fetchReviews && this.props.fetchReviews()
+        }catch (e) {
+            console.log(e)
+        }
+    }
 
     render() {
         const {comment} = this.props
@@ -17,7 +35,11 @@ class Comment extends Component {
                     emptySymbol={<FaRegStar color={"orange"} />}
                 />
                 <span>{comment.comment}</span>
-                <TimeAgo date={comment.createdAt} />
+                <div>
+                    <TimeAgo date={comment.createdAt} />
+                    <Button onClick={()=>this.handleDelete(comment._id)} variant="light"><i>Remove</i></Button>
+                </div>
+
             </div>
         );
     }
