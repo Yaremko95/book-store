9bb5023584f8d3f3d24599091163b1104ef021c7
tetiana <tetianayaremko@gmail.com>
2020-06-06 23:55:03 +0200

    updates comment list

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {Container, Row} from "react-bootstrap";
+import Comment from "./Comment";
+
+class Reviews extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    state={
+        comments:[]
+    }
+    componentDidMount() {
+        this.fetchReviews()
+    }
+
+    fetchReviews = async() => {
+        const {book}= this.props
+        try {
+            let response = await fetch(`https://striveschool.herokuapp.com/api/comments/${book.asin}`, {
+                "method": "GET",
+                "headers": {
+                    "Authorization": "Basic " + btoa('user27:ZGDWyFCA8umbgpvZ')}
+
+            })
+            let comments = await response.json()
+            this.setState({
+                comments:comments
+            })
+        }catch (e) {
+
+        }
+
+
+    }
+
+
+    render() {
+        const {book}=this.props
+        const {comments}= this.state
+        console.log(comments)
+        return (
+            <>
+                <h3 className={"mt-4"}>{comments.length} Reviews</h3>
+
+                  {comments.map(comment=> (
+                      <Comment comment={comment} fetchReviews={this.fetchReviews}/>
+                      )
+                  )}
+
+            </>
+        );
+    }
+}
+
+export default Reviews;
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import booksList from '../data/fantasy.json'
+import {Col, Container, Row, Image} from "react-bootstrap";
+import Reviews from "../components/Reviews";
 
 class Book extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    state = {
+      book:booksList.find(book => book.asin ==this.props.match.params.bookId )
+    }
+
     render() {
         return (
-            <div>
-                   <h1>Book id is: {this.props.match.params.bookId}</h1>
-            </div>
+          <Container className={"mt-5 p-5 "} style={{
+              border:"2px solid #EBEFF2"
+          }} >
+             <Row>
+                 <Col lg={4}>
+                     <Image src={this.state.book.img} thumbnail style={{height:"30rem", objectFit:"cover"}}/>
+                 </Col>
+                 <Col lg={8} className={"mt-2"}>
+                     <span style={{
+                         fontSize:"30px",
+                         fontWeight:"bold",
+                        }}>{this.state.book.title}</span>
+                 </Col>
+             </Row>
+                <Reviews book={this.state.book}/>
+          </Container>
         );
     }
 }
